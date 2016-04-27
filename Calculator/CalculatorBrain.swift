861f253ab64063f0e8ede03f86d0b5aec5f8034d
Cheng-Lin Yang <clyang@csie.io>
2016-04-27 01:20:29 +0100

    add missing files

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -7,3 +7,73 @@
 //
 
 import Foundation
+
+class CalculatorBrain {
+    private var accumlator = 0.0
+    
+    func setOperand(operand: Double) {
+        accumlator = operand
+    }
+    
+    var operations = [
+        "π": Operation.Constant(M_PI),
+        "e": Operation.Constant(M_E),
+        "C": Operation.ClearValue,
+        "+/-": Operation.UnaryOperation({ $0 * -1 }),
+        "sqrt": Operation.UnaryOperation(sqrt),
+        "sin": Operation.UnaryOperation({sin( $0 * M_PI / 180)}),
+        "cos": Operation.UnaryOperation({cos( $0 * M_PI / 180)}),
+        "log": Operation.UnaryOperation(log10),
+        "×": Operation.BinaryOperation({ $0 * $1}),
+        "+": Operation.BinaryOperation({ $0 + $1}),
+        "-": Operation.BinaryOperation({ $0 - $1}),
+        "÷": Operation.BinaryOperation({ $0 / $1}),
+        "=": Operation.Equals
+    ]
+    
+    enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+        case ClearValue
+    }
+    
+    struct PendingBinaryOperationInfo {
+        var binaryFunction: ((Double, Double) ->  Double)
+        var firstOperand: Double
+    }
+    
+    private var pending: PendingBinaryOperationInfo?
+
+    func performOperation(symbol: String) {
+        if let opeation = operations[symbol] {
+            switch opeation{
+            case .Constant(let value):
+                accumlator = value
+            case .UnaryOperation(let function):
+                accumlator = function(accumlator)
+            case .BinaryOperation(let function):
+                executePendingBinaryOperation()
+                pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumlator)
+            case .Equals:
+                executePendingBinaryOperation()
+            case .ClearValue:
+                accumlator = 0
+                pending = nil
+            }
+        }
+    }
+    
+    private func executePendingBinaryOperation() {
+        if pending != nil {
+            accumlator = pending!.binaryFunction(pending!.firstOperand, accumlator)
+            pending = nil
+        }    }
+    
+    var result: Double {
+        get {
+            return accumlator
+        }
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,117 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet private weak var display: UILabel!
+    @IBOutlet private weak var performedProcess: UILabel!
+    private var userIsIntheMiddleOfTyping = false
+    private var userPressFloatPoint = false
+    
+    @IBAction private func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsIntheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            if textCurrentlyInDisplay == "0" && digit != "0" && digit != "⬅︎"{
+                if digit == "." {
+                    display.text = "0."
+                } else {
+                    display.text = digit
+                }
+            } else if textCurrentlyInDisplay != "0" && digit != "⬅︎" {
+                if digit == "." && userPressFloatPoint == false  {
+                    display.text = textCurrentlyInDisplay + digit
+                    userPressFloatPoint = true
+                } else if digit != "." {
+                    display.text = textCurrentlyInDisplay + digit
+                }
+            } else if digit == "⬅︎" {
+                if textCurrentlyInDisplay.characters.count > 1 {
+                    if textCurrentlyInDisplay.characters.last == "." {
+                        userPressFloatPoint = false
+                    }
+                    display.text = textCurrentlyInDisplay.substringToIndex(textCurrentlyInDisplay.endIndex.predecessor())
+                } else {
+                    display.text = "0"
+                }
+            }
+        } else {
+            if digit == "." {
+                userPressFloatPoint = true
+                display.text = "0."
+                userIsIntheMiddleOfTyping = true
+            } else if digit != "⬅︎" {
+                display.text = digit
+                userIsIntheMiddleOfTyping = true
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            if floor(newValue) == newValue {
+                display.text = String(Int(newValue))
+            } else {
+                display.text = String(newValue)
+            }
+        }
     }
-
-
+    
+    private var isProcessable: Bool {
+        get {
+            return display.text! != "-"
+            
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    @IBAction private func performOperation(sender: UIButton) {
+        
+        if userIsIntheMiddleOfTyping && isProcessable {
+            brain.setOperand(displayValue)
+            if floor(displayValue) == displayValue {
+                performedProcess.text = performedProcess.text! + " " + String(Int(displayValue))
+            } else {
+                performedProcess.text = performedProcess.text! + " " + String(displayValue)
+            }
+        }
+        
+        if let symbols = sender.currentTitle {
+            if symbols == "C" {
+                performedProcess.text = ""
+                userPressFloatPoint = false
+                userIsIntheMiddleOfTyping = false
+                brain.performOperation(symbols)
+                displayValue = brain.result
+            } else {
+                if symbols != "=" {
+                    let replacedString = performedProcess.text!.stringByReplacingOccurrencesOfString("=", withString: "")
+                    performedProcess.text = replacedString + " " + symbols + " ="
+                    if symbols == "+/-" {
+                        if userIsIntheMiddleOfTyping {
+                            displayValue = displayValue * -1
+                        } else {
+                            brain.performOperation(symbols)
+                            displayValue = brain.result
+                            userIsIntheMiddleOfTyping = false
+                            userPressFloatPoint = false
+                        }
+                    } else if isProcessable {
+                        brain.performOperation(symbols)
+                        displayValue = brain.result
+                        userIsIntheMiddleOfTyping = false
+                    }
+                } else if symbols == "=" && isProcessable {
+                    brain.performOperation(symbols)
+                    displayValue = brain.result
+                    userIsIntheMiddleOfTyping = false
+                    userPressFloatPoint = false
+                }
+            }
+        }
+        
+    }
 }
 
